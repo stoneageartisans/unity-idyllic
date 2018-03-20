9507f24cc2c448a0c12f5ce8959a7a73ab256717
stoneageartisans <william.a.mann@hotmail.com>
2018-03-20 00:33:21 -0500

    Added pinkish tinting to sky around sunrise and sunset.

--- Assets/Stone Age Artisans/Scripts/Sun.cs
+++ Assets/Stone Age Artisans/Scripts/Sun.cs
@@ -10,10 +10,8 @@ public class Sun : MonoBehaviour
     Material sky;
 
     float[,] atmosphereThickness = new float[24, 60];
-
-    float[,,] sunX = new float[24, 60, 60];
-    float sunY = -90.0f;
-    float sunZ= 0.0f;
+    Color[,] skyTint = new Color[24, 60];
+    Vector3[,,] lightDirection = new Vector3[24, 60, 60];
 
     void Awake()
     {
@@ -29,31 +27,71 @@ public class Sun : MonoBehaviour
 
 	void Start()
     {
+        light = gameObject.GetComponent(typeof(Light)) as Light;
         sky = (Camera.main.gameObject.GetComponent(typeof(Skybox)) as Skybox).material;
 
-        light = gameObject.GetComponent(typeof(Light)) as Light;
+        float redIncrement = 0.01f;
+        float blueIncrement = 0.01f;
+        float currentRed = 0.5f;
+        float currentBlue = 0.5f;
 
-        float currentThickness = 0.998004f;
         float thicknessModifier = 1.002f;
+        float currentThickness = 1.0f / thicknessModifier;
+
         float rotationIncrement = (365.0f / 24.0f / 60.0f / 60.0f);
-        float currentRotation = 270.0f - rotationIncrement;
+        float currentRotationX = 270.0f - rotationIncrement;
 
         for(int hour = 0; hour < 24; hour ++)
         {
             for(int minute = 0; minute < 60; minute ++)
             {
-                if((hour == 0 && minute == 0) || (hour == 12 && minute == 0))
+                // From 4:45 to 5:14 OR 17:45 to 18:14
+                if(((hour == 4 || hour == 17) && minute > 44) || ((hour == 5 || hour == 18) && minute < 15))
                 {
-                    currentThickness = 0.998004f;
+                    if(currentRed > 0.20f)
+                    {
+                        currentRed -= redIncrement;
+                    }
+
+                    if(currentBlue > 0.20f)
+                    {
+                        currentBlue -= blueIncrement;
+                    }
+                }
+                // From 5:15 to 5:44 OR 18:15 to 18:44
+                else if((hour == 5 || hour == 18) && (minute > 14 && minute < 45))
+                {
+                    if(currentRed < 0.5f)
+                    {
+                        currentRed += redIncrement;
+                    }
+
+                    if(currentBlue < 0.5f)
+                    {
+                        currentBlue += blueIncrement;
+                    }
+                }
+                else
+                {
+                    currentRed = 0.5f;
+                    currentBlue = 0.5f;
                 }
 
-                // From 00:00 AM to 6:00 OR from 12:00 to 18:00
+                skyTint[hour, minute] = new Color(currentRed, 0.5f, currentBlue);
+
+                // At 00:00 AND 12:00
+                if((hour == 0 && minute == 0) || (hour == 12 && minute == 0))
+                {                    
+                    currentThickness = 1.0f / thicknessModifier;
+                }
+
+                // From 00:00 AM to 6:00 OR 12:00 to 18:00
                 if((hour >= 0 && hour < 6) || (hour >= 12 && hour < 18))
                 {
                     currentThickness *= thicknessModifier;
                 }
                 else
-                {
+                {                    
                     currentThickness /= thicknessModifier;
                 }
 
@@ -61,26 +99,28 @@ public class Sun : MonoBehaviour
 
                 for(int second = 0; second < 60; second++)
                 {
-                    currentRotation += rotationIncrement;
+                    currentRotationX += rotationIncrement;
 
-                    if(currentRotation >= 360.0f)
+                    if(currentRotationX >= 360.0f)
                     {
-                        currentRotation = 0.0f;
+                        currentRotationX = 0.0f;
                     }
 
-                    sunX[hour, minute, second] = currentRotation;
+                    lightDirection[hour, minute, second] = new Vector3(currentRotationX, -90.0f, 0.0f);
                 }
             }
         }
 
-        setPosition();
+        setLightDirection();
         setAtmosphereThickness();
+        setSkyTint();
 	}
 
     void Update()
     {
-        setPosition();
+        setLightDirection();
         setAtmosphereThickness();
+        setSkyTint();
     }
 
     void setAtmosphereThickness()
@@ -88,12 +128,13 @@ public class Sun : MonoBehaviour
         sky.SetFloat("_AtmosphereThickness", atmosphereThickness[GameTime.instance.dateTime.Hour, GameTime.instance.dateTime.Minute]);
     }
 
-    void setPosition()
+    void setLightDirection()
     {
-        light.transform.eulerAngles = new Vector3(
-            sunX[GameTime.instance.dateTime.Hour, GameTime.instance.dateTime.Minute, GameTime.instance.dateTime.Second],
-            sunY,
-            sunZ
-        );
+        light.transform.eulerAngles = lightDirection[GameTime.instance.dateTime.Hour, GameTime.instance.dateTime.Minute, GameTime.instance.dateTime.Second];
+    }
+
+    void setSkyTint()
+    {
+        sky.SetColor("_SkyTint", skyTint[GameTime.instance.dateTime.Hour, GameTime.instance.dateTime.Minute]);
     }
 }
